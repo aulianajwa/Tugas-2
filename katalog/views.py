e5e5574eecb0c60c172d53affcea7c72e7ca2c0e
aulianajwa <auliesl99@gmail.com>
2022-09-14 11:20:13 +0700

    katalog app

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request) :
+    data_item = CatalogItem.objects.all()
+    context = {
+        'list_item' :data_item,
+        'nama' : 'Aulia Najwa Salsabila',
+        'npm' : '2106751524'
+    }
+    return render(request, "katalog.html", context)
